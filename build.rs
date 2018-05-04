12a0d331cee66fb4fcdb830391cbacefd2a22166
Ben Stuart <mail@bstuart.de>
2018-05-04 19:37:16 +0200

    Add license and automatic README generation

--- build.rs
+++ build.rs
@@ -20,7 +20,25 @@ struct BoardData {
 }
 
 
+fn update_readme() {
+    use std::process::Command;
+    use std::fs::File;
+    use std::io::Write;
+
+    let output = Command::new("cargo").arg("readme").output().unwrap();
+    
+    let readme = String::from_utf8_lossy(&output.stdout);
+
+    File::create("README.md").as_mut().map(|file| {
+        file.write(readme.as_bytes()).unwrap();
+    }).unwrap();
+}
+
 fn main() {
+    println!("cargo:rerun-if-changed:src/lib.rs");
+    println!("cargo:rerun-if-changed:config/board.toml");
+    update_readme();
+
     let mut settings = config::Config::new();
     settings
         .merge(config::File::with_name("config/board"))
